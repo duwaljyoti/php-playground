f99323f665137261e61969a1dadbccca2cb25fda
Jyoti Duwal <duwaljyoti16@gmail.com>
2019-03-31 16:49:53 +0545

    added facade design pattern.

---
+++ design-patterns/facade.php
@@ -0,0 +1,8 @@
+<?php
+
+
+echo "Facade pattern.";
+
+// provies an easy way to access any class without accesing its complexities.
+// for example customer calling call center for various operations like
+// order fulfillment, billing or shipping
---
+++ oop/function-overloading.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Function overloading.<pre>";
+
+class TestClass {
+
+    public function __call($arg1, $arg2) {
+        $numberOfArguments = func_num_args();
+        $args = func_get_args();
+        
+        if ($numberOfArguments === 1) {
+            $this->oneArgMethod(...$args[1]);
+        }
+
+        if ($numberOfArguments === 2) {
+            $this->twoArgsMethod(...$args[1]);
+        }
+    }
+
+    public function oneArgMethod($arg) {
+        var_dump('one argument.');
+    }
+
+    public function twoArgsMethod($arg1, $arg2) {
+        var_dump('two arguments.', $arg1, $arg2);
+    }
+}
+
+$testObject = new TestClass();
+
+$testObject->testMethod('first', 'second');
